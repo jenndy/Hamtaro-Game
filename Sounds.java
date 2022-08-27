fa47ba6057d82cc5acfedb04e242e0d3dd166fc5
Jennifer Dong <33203833+jenndy@users.noreply.github.com>
2022-08-27 18:00:24 -0400

    Update Sounds.java

--- Sounds.java
+++ Sounds.java
@@ -1,6 +1,7 @@
+// Audio input 
+
 import java.io.File;
 import java.io.IOException;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -13,6 +14,7 @@ public class Sounds {
 	public static Clip clip;
 	public static AudioInputStream audioIn;
 	
+	// Constructor that takes in a sound file 
 	protected Sounds(String filename) {
 		sound = new File(filename);
 
@@ -40,7 +42,57 @@ public class Sounds {
 		}
 	}
 	
-	/*public void play() {
-		clip.start();
-	}*/
+	// Sound file for winning 
+	public static class WinS extends Sounds{
+
+		// Constructor 
+		public WinS() {
+			super("sounds/win.wav");
+		}
+	}
+	
+	// Sound file for transitioning between rounds 
+	public static class TransitionS extends Sounds {
+		
+		// Constructor 
+		public TransitionS() {
+			super("sounds/transition.wav");
+		}
+	}
+	
+	// Sound file for losing 
+	public static class LoseS extends Sounds{
+
+		// Constructor 
+		public LoseS() {
+			super("sounds/lose.wav");
+		}
+	}
+	
+	// Sound file for hamster falling out of screen 
+	public static class FallS extends Sounds{
+
+		// Constructor 
+		public FallS() {
+			super("sounds/fall.wav");
+		}
+	}
+	
+	// Sound file for hamster bumping into side of screen or pillow 
+	public static class BumpS extends Sounds {
+		
+		// Constructor 
+		public BumpS() {
+			super("sounds/bump.wav");
+		}
+	}
+	
+	// Sound file for background music 
+	public static class BgMusicS extends Sounds{
+		
+		// Constructor 
+		public BgMusicS() {
+			super("sounds/bg.wav");
+		}
+	}
 }
