1cff8573e1938ec45f7c71183311faabb44d5504
Jennifer Dong <33203833+jenndy@users.noreply.github.com>
2022-08-27 18:10:58 -0400

    Delete BumpS.java

--- BumpS.java
+++
@@ -1,8 +0,0 @@
-//sound file for when the hamster bumps into the side of the screen or the pillow 
-
-public class BumpS extends Sounds {
-	
-	public BumpS() {
-		super("src/sounds/bump.wav");
-	}
-}
