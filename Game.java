239c126a7512ad632194e3f42928351bdf40a59a
Jennifer Dong <33203833+jenndy@users.noreply.github.com>
2022-08-27 17:23:10 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+// Main game class
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -9,7 +11,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
-
 import javax.swing.Timer;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -17,12 +18,17 @@ import javax.swing.JPanel;
 
 public class Game extends JFrame implements ActionListener, KeyListener{
 	
+	// Frame size 
 	public static int width = 600;
 	public static int height = 600; 
+	
+	// Border lines for frame 
 	public static int baseline = 580;
 	public static int topline = 20;
 	public static int leftline = 10;
 	public static int rightline = 520;
+	
+	// Main game variables 
 	public Canvas canvas;
 	public static Timer timer; 
 	public static Lob lob = new Lob(); 
@@ -31,7 +37,6 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 	public static Countdown countdown;
 	public static Lives lives;
 	public static Score score;
-
 	public static double time = 0;
 	public BgMusicS bgMusicS;
 	
@@ -54,19 +59,17 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 		timer.start();
 		
 		lob = new Lob();
-	
 		paddle = new Paddle();
 		
-		r1acorn1 = new Acorn(); // Create in constructor (5) 
+		r1acorn1 = new Acorn();
 		r2acorn1 = new Acorn();
 		r2acorn2 = new Acorn();
-		r3acorn1 = new Acorn(); 
+		r3acorn1 = new Acorn();
 		r3acorn2 = new Acorn();
 		r3acorn3 = new Acorn();
 		
 		countdown = new Countdown();
 		lives = new Lives();
-		
 		bgMusicS = new BgMusicS();
 	}
 	
@@ -80,10 +83,11 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 		@Override
 		public void paintComponent(Graphics g) {
 			
+			// Changes background depending on the round 
 			switch(Score.round) {
 			case 1:
 				drawBG1(g);
-				r1acorn1.draw(g); // Draw in paint component (5)  
+				r1acorn1.draw(g);
 				break;
 			case 2:
 				drawBG2(g);
@@ -110,10 +114,12 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 			g.setColor(Color.GREEN);
 			countdown.draw(g);
 		
+			// Draws losing picture 
 			if (lob.gameOver == true) {
 				score.draw(g);
 			}	
 		
+			// Draws winning picture 
 			if (lob.win == true) {
 				score.draw2(g);
 			}	
@@ -133,7 +139,7 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 		
 		switch(Score.round) {
 		case 1:
-			r1acorn1.update(); // Update in actionPerformed (5) 
+			r1acorn1.update();
 			break;
 		case 2:
 			r2acorn1.update();
@@ -157,25 +163,21 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		//System.out.println("Pressed");
 		int keyCode = e.getKeyCode();
+		
+		// Determines paddle movement 
 		switch( keyCode ) {
 		case KeyEvent.VK_UP:
-			//System.out.println("Up");
 			paddle.vY -= 10; 
 			break;
 		case KeyEvent.VK_DOWN:
-			//System.out.println("Down");
 			paddle.vY += 10;
 			break;
 		case KeyEvent.VK_LEFT:
-			//System.out.println("Left");
 			paddle.vX -= 10;
 			break;
 		case KeyEvent.VK_RIGHT:
-			//System.out.println("Right");
 			paddle.vX += 10;
-			//System.out.println(paddle.vX);
 			break;
 		}
 		repaint();
@@ -183,12 +185,14 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		//System.out.println("Released");
+		
+		// Sets paddle velocity to 0 when key is not pressed 
 		paddle.vX = 0;
 		paddle.vY = 0;
 		
 	}
 	
+	// Reads in background images
 	protected Image loadImage(String filename) {
 		try {
 			return ImageIO.read(new File(filename));
@@ -202,6 +206,7 @@ public class Game extends JFrame implements ActionListener, KeyListener{
 	Image bg2 = loadImage("src/graphics/bg2.png");
 	Image bg3 = loadImage("src/graphics/bg3.png");
 	
+	// Draws background images 
 	public void drawBG1(Graphics g) {
 		g.drawImage(bg1, 0, 0, null);
 	}
